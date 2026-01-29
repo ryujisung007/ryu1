7c269805e2aad9ea283321ec417457a050376148
ryujisung007 <118189840+ryujisung007@users.noreply.github.com>
2026-01-29 09:52:06 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -272,4 +272,4 @@ for col, t in zip(cols, top5):
         if img_url:
             st.image(img_url, use_container_width=True)
         else:
-            st.markdown("<div style='height:200px;background:#eee;text-align:center;line-height:200px;'>이미지 없음</d
+            st.markdown("<div style='height:200px;background:#eee;text-align:center;line-height:200px;'>이미지 없음</d>
